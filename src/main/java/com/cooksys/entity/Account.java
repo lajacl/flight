e4042d3b10de2945c4a9972ac74fd731923907e2
LaVette Clifton <lavette.clifton@gmail.com>
2017-07-18 15:46:23 -0500

    java account, flight entities creation and database annotation setup

---
+++ src/main/java/com/cooksys/entity/Account.java
@@ -0,0 +1,106 @@
+package com.cooksys.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Account")
+public class Account {
+		
+	@Id
+	@GeneratedValue
+	private long id;
+
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "firstName")
+	private String firstName;
+
+	@Column(name = "lastName")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "phone")
+	private String phone;
+	
+	@ManyToMany(mappedBy = "accounts")
+	private List<FlightEntity> flights;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public List<FlightEntity> getFlights() {
+		return flights;
+	}
+
+	public void setFlights(List<FlightEntity> flights) {
+		this.flights = flights;
+	}
+	
+}
---
+++ src/main/java/com/cooksys/entity/FlightEntity.java
@@ -0,0 +1,81 @@
+package com.cooksys.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Flight")
+public class FlightEntity {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	//Name of city where flight originates
+	@Column(name = "origin")
+	private String origin;
+	
+	//Name of city where flight lands
+	@Column(name = "destination")
+	private String destination;
+	
+	//How many hours flight is in the air
+	@Column(name = "flightTime")
+	private long flightTime;
+	
+	//How many hours after the start of the day until the flight takes off
+	@Column(name = "offsetTime")
+	private long offset;
+
+	@ManyToMany
+	private List<Account> accounts;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public long getFlightTime() {
+		return flightTime;
+	}
+	public void setFlightTime(long flightTime) {
+		this.flightTime = flightTime;
+	}
+	public long getOffset() {
+		return offset;
+	}
+	public void setOffset(long offset) {
+		this.offset = offset;
+	}
+	public List<Account> getAccounts() {
+		return accounts;
+	}
+	public void setAccounts(List<Account> accounts) {
+		this.accounts = accounts;
+	}
+	public FlightEntity(String origin, String destination, long flightTime, long offset) {
+		super();
+		this.origin = origin;
+	}
+
+}
