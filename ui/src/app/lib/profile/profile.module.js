f207e8f243e1d696ae02d34721cb35e975624741
LaVette Clifton <lavette.clifton@gmail.com>
2018-03-19 21:23:53 -0400

    map module dependency added to profile module

--- ui/src/app/lib/profile/profile.module.js
+++ ui/src/app/lib/profile/profile.module.js
@@ -1,9 +1,12 @@
 import { ProfileService } from './profile.service'
 import { flightProfile } from './profile.component'
 import { config } from './profile.config'
+import flightMap from '../map/map.module'
 
 export default angular
-  .module('flight.profile', [])
+  .module('flight.profile', [
+    flightMap
+  ])
   .service('profileService', ProfileService)
   .component('flightProfile', flightProfile)
   .config(config)
